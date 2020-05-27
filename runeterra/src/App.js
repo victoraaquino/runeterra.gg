e4d69782a7afa26660f66c680c2c93b8dc7b3f1a
Victor Aquino <kirito-aquino@outrlook.com>
2020-05-26 22:19:58 -0300

    filtragem das cartas

--- runeterra/src/App.js
+++ runeterra/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import Menu from './menu/Menu';
+
+import cardsJson from './lor_api/set1-en_us.json';
+import cardsJson2 from './lor_api/set1-en_us.json';
+import regionsJson from './lor_api/globals-en_us.json';
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const cardsJsonFull = cardsJson.concat(cardsJson2);
+  const [cards, setCards] = useState(cardsJsonFull);
+  const [regions, SetRegions] = useState(regionsJson.regions);
+
+  let text = '';
+
+  function handleFilterCards(value) {
+    if(value === ""){
+      setCards(cardsJsonFull);
+    }else{
+      setCards(cardsJsonFull.filter(card => card.regionRef === value));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Menu />
+      <div className="container-cards" >
+        <div className="container-filtros">
+          <select className="inputs" onChange={event => handleFilterCards(event.target.value)} >
+            <option value="">Selecione</option>
+            {regions.map(region => <option value={region.nameRef}>{region.name}</option>)}
+          </select>
+        </div>
+        {cards.length === 0 ?  <h4>Não possui Cards.</h4> : ''}
+        {cards.map(card => {
+            return <img src={card.assets[0].gameAbsolutePath} className="card-img" />
+        })}
+      </div>
+    </>
   );
 }
-
-export default App;
